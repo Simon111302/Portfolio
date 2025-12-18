dfa31d8d197c16d97dec6ce8f60df1b657490112
Simon111302 <probelenia9@gmail.com>
2025-12-18 23:41:11 +0800

    icons

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,19 @@
+import { FaHome, FaTools, FaProjectDiagram, FaUser, FaEnvelope } from 'react-icons/fa';
+
 function Navbar() {
     return (
       <nav className="navbar">
-        <div className="nav-container">
-          <h2 className="logo">Portfolio</h2>
-          <ul className="nav-links">
-            <li><a href="#home">Home</a></li>
-            <li><a href="#tools">Tools</a></li>
-            <li><a href="#projects">Project</a></li>
-            <li><a href="#about">About</a></li>
-            <li><a href="#contact">Contact</a></li>
-          </ul>
-        </div>
-      </nav>
+      <div className="nav-container">
+        <h2 className="logo">Portfolio</h2>
+        <ul className="nav-links">
+          <li><a href="#home"><FaHome/> Home</a></li>
+          <li><a href="#tools"><FaTools /> Tools</a></li>
+          <li><a href="#projects"><FaProjectDiagram /> Project</a></li>
+          <li><a href="#about"><FaUser /> About</a></li>
+          <li><a href="#contact"><FaEnvelope /> Contact</a></li>
+        </ul>
+      </div>
+    </nav>
     )
   }
   export default Navbar
