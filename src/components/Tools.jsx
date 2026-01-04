4aca56c16209a3eaab3f7fb9421d6d764eab7b3a
Simon111302 <probelenia9@gmail.com>
2026-01-04 09:05:41 +0800

    update

--- src/components/Tools.jsx
+++ src/components/Tools.jsx
@@ -145,7 +145,6 @@ function Tools() {
          </div>
         </div>
         <div className="tools-about-right">
-         <h3>About Me</h3>
          <p>
            I'm a student developer passionate about building web applications using modern technologies. I'm eager to learn more about software development and continuously grow as a developer. I'm a passionate student developer dedicated to building innovative web applications using modern technologies. I'm committed to continuous learning and professional growth in the field of software development. My journey is driven by curiosity, a love for problem-solving, and the desire to create meaningful digital solutions that make an impact.
          </p>
