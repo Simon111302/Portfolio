00381b5673dbec4aa363315453661cf577da98e3
Paul Simon Belenia <probelenia9@gmail.com>
2026-01-04 09:41:46 +0800

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ function Footer() {
     return (
       <footer className="footer">
         <div className="footer-content">
-          <p>&copy; 2025 Paul Simon Belenia. All rights reserved.</p>
+          <p>&copy; 2026 Paul Simon Belenia. All rights reserved.</p>
           <div className="social-links">
           </div>
         </div>
